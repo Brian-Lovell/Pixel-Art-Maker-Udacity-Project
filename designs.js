93adb4b1e6442a1ecbefe73f7fd3c7662fe3720c
Brian Lovell <brian.lovell@outlook.com>
2021-04-04 12:33:03 -0400

    How do I make this work lol

--- designs.js
+++ designs.js
@@ -1,21 +1,12 @@
 // Select color input
-var color_input = document.getElementById(colorPicker);
+var color_input = document.getElementById('colorPicker');
 // Select size input
-var canvas_height = document.getElementById(inputHeight);
-var canvas_width = document.getElementById(inputWidth);
-// Select Canvas
-var canvas = document.getElementById(pixelCanvas);
+var canvas_height = document.getElementById('inputHeight');
+var canvas_width = document.getElementById('inputWidth');
 
 // When size is submitted by the user, call makeGrid()
-var submit_button = document.getElementById(submitButton);
+var submit_button = document.getElementById('submitButton');
 submit_button.addEventListener("click", makeGrid);
 
 
-function makeGrid() {
-    for (let i = 0; i < canvas_height; i++) {
-        canvas.insertRow(i);
-    }
-        for (let x = 0; x < canvas_width; x++) {
-            canvas.insertCell(x);
-        }
-}
+function makeGrid
