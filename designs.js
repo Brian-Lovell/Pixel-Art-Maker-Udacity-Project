b1f6f2578b52814bc9a3a94b55ceed5b07270aff
Brian Lovell <brian.lovell@outlook.com>
2021-04-04 11:16:03 -0400

    Add variables to store elements

--- designs.js
+++ designs.js
@@ -1,6 +1,9 @@
 // Select color input
+var color_input = document.getElementById(colorPicker);
 // Select size input
-
+var size_input = document.getElementById(sizePicker);
+// Select Canvas
+var canvas = document.getElementById(pixelCanvas);
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
