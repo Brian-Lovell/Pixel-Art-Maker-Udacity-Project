7d300045fb705ac8c956b200a5d4348b9b936244
Brian Lovell <brian.lovell@outlook.com>
2021-04-04 11:35:26 -0400

    Remove variable size picker

--- designs.js
+++ designs.js
@@ -1,12 +1,14 @@
 // Select color input
 var color_input = document.getElementById(colorPicker);
 // Select size input
-var size_input = document.getElementById(sizePicker);
-// Select Canvas
-var canvas = document.getElementById(pixelCanvas);
 var canvas_height = document.getElementById(inputHeight);
 var canvas_width = document.getElementById(inputWidth);
+// Select Canvas
+var canvas = document.getElementById(pixelCanvas);
+
 // When size is submitted by the user, call makeGrid()
+
+
 
 function makeGrid() {
 
