8a4b1d2497f71716664d5a7ce677c03eda36e339
Brian Lovell <brian.lovell@outlook.com>
2021-04-04 12:14:33 -0400

    Update variables with quotes

--- designs.js
+++ designs.js
@@ -7,15 +7,15 @@ var canvas_width = document.getElementById(inputWidth);
 var canvas = document.getElementById(pixelCanvas);
 
 // When size is submitted by the user, call makeGrid()
-var submit_button = document.querySelector("submit");
-submit_button.addEventListener('click', makeGrid);
+var submit_button = document.getElementById(submitButton);
+submit_button.addEventListener("click", makeGrid);
 
 
 function makeGrid() {
     for (let i = 0; i < canvas_height; i++) {
-        canvas.insertRow()
+        canvas.insertRow(i);
     }
         for (let x = 0; x < canvas_width; x++) {
-            canvas.insertCell()
+            canvas.insertCell(x);
         }
 }
